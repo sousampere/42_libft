ba2edff750a9f0a089fc687b176150880c0a95b2
Gaspard TOURDIAT <gaspard.tourdiat@gmail.com>
2025-11-05 19:44:08 +0100

    add(functions) lstsize

--- ft_lstsize.c
+++ ft_lstsize.c
@@ -6,7 +6,7 @@
 /*   By: gtourdia <@student.42mulhouse.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/05 17:18:44 by gtourdia          #+#    #+#             */
-/*   Updated: 2025/11/05 17:26:11 by gtourdia         ###   ########.fr       */
+/*   Updated: 2025/11/05 19:43:18 by gtourdia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,27 @@
 int	ft_lstsize(t_list *lst)
 {
 	int	i;
+	t_list	*nextl;
 
 	i = 0;
-	while (lst->next != NULL)
+	if (lst == NULL)
+		return (0);
+	if (lst->next == NULL)
+		return (1);
+	nextl = lst->next;
+	while (nextl->next != NULL)
 	{
 		i++;
-		lst++;
+		lst = lst->next;
 	}
-	return (i);
+	return (i + 2);
 }
+
+// #include <stdio.h>
+// int main()
+// {
+// 	t_list * l =  NULL;
+// 	ft_lstadd_front(&l, ft_lstnew((void*)1));
+// 	ft_lstadd_front(&l, ft_lstnew((void*)2));
+// 	printf("%d\n", ft_lstsize(l));
+// }
