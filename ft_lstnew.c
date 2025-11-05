c35cc9d8465abfffbcc8b4c7fe66926932d2dbba
Gaspard TOURDIAT <gaspard.tourdiat@gmail.com>
2025-11-05 13:44:28 +0100

    add(functions) added lstnew

---
+++ ft_lstnew.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gtourdia <@student.42mulhouse.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/05 13:23:49 by gtourdia          #+#    #+#             */
+/*   Updated: 2025/11/05 13:40:40 by gtourdia         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstnew(void *content)
+{
+	t_list	*list;
+
+	list = malloc(sizeof(t_list));
+	list->content = content;
+	list->next = NULL;
+	return (list);
+}
