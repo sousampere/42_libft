8370f3e6675bf9ef381d5ff38388f608c3182464
Gaspard TOURDIAT <gaspard.tourdiat@gmail.com>
2025-11-05 12:23:00 +0100

    update(putnbr_fd) remade without malloc

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,7 +6,7 @@
 /*   By: gtourdia <@student.42mulhouse.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/31 15:16:01 by gtourdia          #+#    #+#             */
-/*   Updated: 2025/11/01 16:36:49 by gtourdia         ###   ########.fr       */
+/*   Updated: 2025/11/05 12:20:52 by gtourdia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,32 @@
 
 void	ft_putnbr_fd(int n, int fd)
 {
-	char	*str;
-
-	str = ft_itoa(n);
-	write(fd, str, ft_strlen(str));
+	char	c;
+	
+	if (n == -2147483648)
+	{
+		write(fd, "-2147483648", 12);
+		return ;
+	}
+	if (n == 2147483647)
+	{
+		write(fd, "2147483647", 11);
+		return ;
+	}
+	if (n < 0)
+	{
+		write(fd, "-", 1);
+		n *= -1;
+	}
+	if (n >= 10)
+		ft_putnbr_fd(n / 10, fd);
+	c = (n % 10) + 48;
+	write(fd, &c, 1);
 }
+
+// int main()
+// {
+// 	int fd = open("file.txt", O_RDWR);
+// 	ft_putnbr_fd(0, fd);
+// 	close(fd);
+// }
