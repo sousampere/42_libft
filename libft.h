633dd8502fffaa898c5ab7dbed94f56e12fa27bd
Gaspard TOURDIAT <gaspard.tourdiat@gmail.com>
2025-11-06 14:04:22 +0100

    add(function) lstiter

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: gtourdia <@student.42mulhouse.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/27 15:08:31 by gtourdia          #+#    #+#             */
-/*   Updated: 2025/11/06 10:34:05 by gtourdia         ###   ########.fr       */
+/*   Updated: 2025/11/06 14:03:31 by gtourdia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -79,5 +79,6 @@ int		ft_lstsize(t_list *lst);
 t_list	*ft_lstlast(t_list *lst);
 void	ft_lstdelone(t_list *lst, void (*del)(void *));
 void	ft_lstclear(t_list **lst, void (*del)(void *));
+void	ft_lstiter(t_list *lst, void (*f)(void *));
 
 #endif
