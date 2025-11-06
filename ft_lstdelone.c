923c80cac6c3ac50376304ebf4e406b99ec1dfa1
Gaspard TOURDIAT <gaspard.tourdiat@gmail.com>
2025-11-06 10:23:49 +0100

    add(function) lstdelone

--- ft_lstdelone.c
+++ ft_lstdelone.c
@@ -6,7 +6,7 @@
 /*   By: gtourdia <@student.42mulhouse.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/05 22:31:00 by gtourdia          #+#    #+#             */
-/*   Updated: 2025/11/05 22:50:47 by gtourdia         ###   ########.fr       */
+/*   Updated: 2025/11/06 10:21:47 by gtourdia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,6 @@
 
 void	ft_lstdelone(t_list *lst, void (*del)(void *))
 {
-	t_list	*ptr;
-
-	ptr = lst->next;
-	del((void *) lst);
-	lst = ptr;
+	del((void *)lst->content);
+	free(lst);
 }
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: gtourdia <@student.42mulhouse.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/27 15:08:31 by gtourdia          #+#    #+#             */
-/*   Updated: 2025/11/05 20:02:29 by gtourdia         ###   ########.fr       */
+/*   Updated: 2025/11/06 10:07:34 by gtourdia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -77,5 +77,6 @@ void	ft_lstadd_front(t_list **lst, t_list *new);
 void	ft_lstadd_back(t_list **lst, t_list *new);
 int		ft_lstsize(t_list *lst);
 t_list	*ft_lstlast(t_list *lst);
+void	ft_lstdelone(t_list *lst, void (*del)(void *));
 
 #endif
