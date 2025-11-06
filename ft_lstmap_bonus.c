c95ab1e33b50727e60b2746266a72c6c923956a6
Gaspard TOURDIAT <gaspard.tourdiat@gmail.com>
2025-11-06 16:56:15 +0100

    update(bonus) changed bonus name

---
+++ ft_lstmap_bonus.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstmap_bonus.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gtourdia <@student.42mulhouse.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/06 14:04:46 by gtourdia          #+#    #+#             */
+/*   Updated: 2025/11/06 15:50:59 by gtourdia         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+// void	free_all(t_list *lst, int size)
+// {
+	
+// }
+
+// t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+// {
+// 	t_list	*new;
+// 	int		i;
+// 	int		size;
+	
+// 	if (lst == NULL)
+// 		return (NULL);
+// 	new = ft_lstnew(f(lst->content));
+// 	if (new == NULL)
+// 		return (NULL);
+// 	size = 
+// 	while (condition)
+// 	{
+// 		/* code */
+// 	}
+	
+// }
---
+++ ft_lstsize_bonus.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstsize_bonus.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gtourdia <@student.42mulhouse.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/05 17:18:44 by gtourdia          #+#    #+#             */
+/*   Updated: 2025/11/06 16:09:10 by gtourdia         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_lstsize(t_list *lst)
+{
+	int	i;
+	t_list	*nextl;
+
+	i = 0;
+	if (lst == NULL)
+		return (0);
+	if (lst->next == NULL)
+		return (1);
+	nextl = lst->next;
+	while (nextl->next != NULL)
+	{
+		i++;
+		lst = lst->next;
+		nextl = lst->next;
+	}
+	return (i);
+}
+
+// #include <stdio.h>
+// int main()
+// {
+// 	t_list * l =  NULL;
+// 	ft_lstadd_front(&l, ft_lstnew((void*)1));
+// 	ft_lstadd_front(&l, ft_lstnew((void*)2));
+// 	printf("%d\n", ft_lstsize(l));
+// }
