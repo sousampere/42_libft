00ed4dca17c17c92d8f12be091c9919e2e07c8e0
Gaspard TOURDIAT <gaspard.tourdiat@gmail.com>
2025-11-07 11:37:29 +0100

    update(strtrim) remade working

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: gtourdia <@student.42mulhouse.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/30 18:09:50 by gtourdia          #+#    #+#             */
-/*   Updated: 2025/11/06 22:05:54 by gtourdia         ###   ########.fr       */
+/*   Updated: 2025/11/07 11:36:41 by gtourdia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,46 +26,85 @@ static int	is_in_set(char c, const char *set)
 	return (0);
 }
 
-char	*ft_strtrim(char const *s1, char const *set)
+static int	get_len(char const *s1, const char *set)
 {
-	int	i;
+	size_t	i;
+	size_t	subi;
 	size_t	len;
-	size_t	stri;
-	char	*string;
 
 	i = 0;
+	while (s1[i] && is_in_set(s1[i], set))
+		i++;
 	len = 0;
-	if (s1 == NULL || set == NULL)
-		return (NULL);
-	while (s1[i++])
-		len += !(is_in_set(s1[i], set));
-	if (!(string = malloc(sizeof(char) * (len + 1))))
-		return (NULL);
-	i = 0;
-	stri = 0;
-	while (stri < len)
+	while (s1[i])
 	{
-		if (!is_in_set(s1[i], set))
-			string[stri++] = s1[i];
+		if (is_in_set(s1[i], set))
+		{
+			subi = 0;
+			while (s1[i + subi] && is_in_set(s1[i + subi], set))
+				subi++;
+			if (s1[i + subi] == '\0')
+				return (len);
+		}
+		len++;
 		i++;
 	}
-	string[stri] = '\0';
-	return (string);
+	return (len);
 }
+
+char	*ft_strtrim(char const *s1, char const *set)
+{
+	char	*str;
+	int		i;
+	int		len;
+
+	len = get_len(s1, set);
+	str = malloc(sizeof(char) * (len + 1));
+	i = 0;
+	while (*s1 != '\0' && is_in_set(*s1, set))
+		s1++;
+	while (i < len)
+	{
+		str[i] = s1[i];
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
+
+
+// char	*ft_strtrim(char const *s1, char const *set)
+// {
+// 	size_t	i;
+
+	
+// 	while (s1[i])
+// 	{
+		
+// 	}
+// }
 
 // #include <stdlib.h>
 // #include <string.h>
+// #include "stdio.h"
 // int main() {
-//     char *s1 = "Hello World";
-//     char *set = "l";
+//     char *s1 = "   Hello World   ";
+//     char *set = " ";
 //     char *result = ft_strtrim(s1, set);
 
+// 	printf("%s|||\n", result);
 //     if (result == NULL) return 1;
-//     if (strcmp(result, "  Hello World  ") != 0) {
+//     if (strcmp(result, "Hello World") != 0) {
 //         free(result);
 //         return 1;
 //     }
 
 //     free(result);
 //     return 0;
+// }
+
+// #include "stdio.h"
+// int main()
+// {
+// 	printf("%d", get_len("     Hel    l o ", "\t "));
 // }
